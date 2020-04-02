3edf68de70654d60f5bff04fd70b3b1015061ebe
Jay Yang <hjyang1@uci.edu>
2020-04-02 13:42:50 -0700

    All Files for Exercise 1

--- src/main/java/TermFrequency.java
+++ src/main/java/TermFrequency.java
@@ -1,45 +1,95 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.Dictionary;
-import java.util.HashMap;
-import java.util.Hashtable;
+import java.util.*;
 
 public class TermFrequency
 {
     private static String filePath;
+    private static List<String> stopWords;
+    private static String stopPath = "stop_words.txt";
 
     public static void main( String[] args )
     {
 
         filePath = args[0];
-        HashMap<String, Integer> dict = new HashMap();
+
+
+        HashMap<String, Integer> map = new HashMap();
+
+        generateStopWords();
+
         try{
             BufferedReader reader = new BufferedReader(new FileReader(filePath));
             String line;
             while((line = reader.readLine()) != null){
-                String[] arrOfStr = line.split("[, ?.@]+");
+                String[] arrOfStr = line.trim().split("[^A-Za-z]");
 
                 for (int i = 0; i < arrOfStr.length; i++){
-                    String word = arrOfStr[i];
-                    addWord(dict, word);
+                    String word = arrOfStr[i].toLowerCase();
+                    if (word.length() > 1 && !stopWords.contains(word)){
+                        addWord(map, word);
+                    }
                 }
             }
+
+            List<Map.Entry<String,Integer>> sortedWords = sortByValue(map);
+
+            printResults(sortedWords);
         }
         catch(Exception e){
             System.out.println(e);
         }
 
-
-        System.out.println(filePath);
     }
 
-    private static void addWord(HashMap<String, Integer> dict, String word){
-        if (!dict.containsKey(word)){
-            dict.put(word,1);
+    private static void addWord(HashMap<String, Integer> map, String word){
+
+        if (!map.containsKey(word)){
+            map.put(word,1);
         }
         else{
-            int currentAmount = dict.get(word);
-            dict.replace(word, currentAmount+1);
+            int currentAmount = map.get(word);
+            map.replace(word, currentAmount+1);
+        }
+    }
+
+    private static List<Map.Entry<String,Integer>> sortByValue(HashMap<String, Integer> hashmap){
+        List<Map.Entry<String, Integer>> list = new LinkedList<Map.Entry<String, Integer>>(hashmap.entrySet());
+
+        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>(){
+            public int compare(Map.Entry<String, Integer> o1,
+                               Map.Entry<String, Integer> o2) {
+                return (o2.getValue()).compareTo(o1.getValue());
+            }
+        });
+
+        return list;
+    }
+
+    private static void printResults(List<Map.Entry<String, Integer>> results){
+
+        StringBuilder out = new StringBuilder("-----------Results----------");
+        int counter = 0;
+        for (Map.Entry<String, Integer> entry : results){
+            out.append("\n"+entry.getKey()+":"+entry.getValue());
+            counter++;
+            if(counter > 24){ break; }
+        };
+
+        System.out.println(out.toString());
+    }
+
+    private static void generateStopWords(){
+        try{
+            BufferedReader readStop = new BufferedReader(new FileReader(stopPath));
+            String line;
+            while ((line = readStop.readLine()) != null) {
+                stopWords = Arrays.asList(line.split(","));
+            }
+
+        }
+        catch(Exception e){
+            System.out.println("Could not load Stop Words");
         }
     }
 }
